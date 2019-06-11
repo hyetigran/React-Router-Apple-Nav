3ccef2eecaac43c7df2a44a9c090e3aef2237d58
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-06-11 17:03:34 +0400

    added navData

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,12 +1,45 @@
 import React from 'react';
-import './App.css';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
+import './App.css';
+const navData = [
+	{
+		id: '1',
+		name: 'Mac',
+		products: [
+			{ id: '1', name: 'Macbook' },
+			{ id: '2', name: 'Macbook Air' },
+			{ id: '3', name: 'Macbook Pro' },
+			{ id: '4', name: 'iMac' },
+			{ id: '5', name: 'iMac Pro', isNew: true },
+			{ id: '6', name: 'Mac Pro' },
+			{ id: '7', name: 'Mac mini' },
+			{ id: '8', name: 'Accessories' },
+			{ id: '9', name: 'High Sierra' },
+			{ id: '10', name: 'Compare' }
+		]
+	},
+	{ id: '2', name: 'iPad', products: [] },
+	{ id: '3', name: 'iPhone', products: [] },
+	{ id: '4', name: 'Watch', products: [] },
+	{ id: '5', name: 'TV', products: [] },
+	{ id: '6', name: 'Music', products: [] },
+	{ id: '7', name: 'Support', products: [] }
+];
+
+const StyledContainer = styled.div`
+	padding: 20px;
+	height: 100%;
+
+	nav {
+		padding: 0 20px;
+		display: flex;
+		justify-content: space-between;
+	}
+`;
 function App() {
-  return (
-    <div className="App">
-  
-    </div>
-  );
+	return <StyledContainer />;
 }
 
 export default App;
